88b1168562a23b8bd50bc6ec4cb998a020022988
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-05 19:27:46 +0600

    restart

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,32 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SKT-ASP-Portfolio")]
+[assembly: AssemblyTitle("WebApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SKT-ASP-Portfolio")]
+[assembly: AssemblyProduct("WebApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f28fdebf-c653-461d-b32c-350e552c69a2")]
+[assembly: Guid("87c061ce-f060-4cca-9fff-a3973aff9fcd")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Site.Master.cs
+++ Site.Master.cs
@@ -1,29 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace SKT_ASP_Portfolio
+namespace WebApplication1
 {
-    /// <summary>
-    /// Master page for the SKT Portfolio website
-    /// Provides common layout and navigation for all pages
-    /// </summary>
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Set active navigation item based on current page
-            SetActiveNavigation();
-        }
-
-        /// <summary>
-        /// Highlights the active navigation menu item based on current page
-        /// </summary>
-        private void SetActiveNavigation()
-        {
-            string currentPage = System.IO.Path.GetFileName(Page.Request.Url.AbsolutePath);
-            
-            // Add logic here to highlight active menu item
-            // This can be expanded when we add more sophisticated navigation handling
+            // Master page initialization logic can be added here
         }
     }
 }
